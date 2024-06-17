f9ed0094bfdc85aa949e672ab35c22ab730befd0
Marcus Stong <stongo@gmail.com>
2024-06-17 03:18:30 -0400

    update graviton AMI used for arm builder (#680)

--- .github/operations/terraform/environments/build/main.tf
+++ .github/operations/terraform/environments/build/main.tf
@@ -2,7 +2,7 @@ locals {
   region = "eu-west-3"
   arm = {
     # Buildkite Elastic Stack (Amazon Linux 2023 w/ docker)
-    ami           = "ami-0269085aee1a64cf7"
+    ami           = "ami-0bb87f0dc4192d154"
     instance_type = "t4g.xlarge"
     name          = "kms-core-docker-builder-arm"
     key_name      = "docker_builder_arm"
