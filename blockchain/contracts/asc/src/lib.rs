5e6fcc267031e8043d020681b70426271940d86a
Maksym Surzhynskyi <52411920+maksymsur@users.noreply.github.com>
2024-11-14 13:40:55 +0200

    fix: Remove global allocator for WebAssembly builds that has security implications (#1488)

--- blockchain/contracts/asc/src/lib.rs
+++ blockchain/contracts/asc/src/lib.rs
@@ -1,12 +1,3 @@
-// Configuring wee_alloc as the global allocator for WebAssembly builds
-// This reduces the size of the Wasm binary by about 10KB compared to the default allocator
-#[cfg(target_arch = "wasm32")]
-use wee_alloc;
-
-#[cfg(target_arch = "wasm32")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 pub mod contract;
 mod events;
 mod state;
