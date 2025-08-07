1a82d27d310596969ee1e1d6bd8bba222e5a180d
Titouan Tanguy <titouan.tanguy@zama.ai>
2025-08-07 18:06:00 +0200

    WiP: decompress xof generated standalone keys

--- core/threshold/src/execution/tfhe_internals/lwe_bootstrap_key_generation.rs
+++ core/threshold/src/execution/tfhe_internals/lwe_bootstrap_key_generation.rs
@@ -123,6 +123,7 @@ mod tests {
 
     use itertools::Itertools;
     use tfhe::{
+        boolean::prelude::LweDimension,
         core_crypto::{
             algorithms::{
                 allocate_and_generate_new_lwe_bootstrap_key, decrypt_constant_ggsw_ciphertext,
@@ -145,7 +146,10 @@ mod tests {
         },
         Seed,
     };
-    use tfhe_csprng::{generators::SoftwareRandomGenerator, seeders::Seeder};
+    use tfhe_csprng::{
+        generators::SoftwareRandomGenerator,
+        seeders::{Seeder, XofSeed},
+    };
 
     use crate::{
         algebra::{
@@ -153,7 +157,6 @@ mod tests {
         },
         execution::{
             online::{
-                gen_bits::{BitGenEven, SecureBitGenEven},
                 preprocessing::dummy::DummyPreprocessing,
                 secret_distributions::{RealSecretDistributions, SecretDistributions},
             },
@@ -161,6 +164,7 @@ mod tests {
             runtime::session::{LargeSession, ParameterHandles},
             tfhe_internals::{
                 glwe_key::GlweSecretKeyShare,
+                lwe_bootstrap_key::par_decompress_into_lwe_bootstrap_key_generated_from_xof,
                 lwe_key::LweSecretKeyShare,
                 parameters::{EncryptionType, TUniformBound},
                 randomness::{
@@ -185,36 +189,28 @@ mod tests {
         let t_uniform_bound_glwe = 5_usize;
         let bk_base_log = 18_usize;
         let bk_level_count = 3_usize;
-        let seed = 0;
+        let seed = 42;
 
         let num_key_bits_lwe = lwe_dimension;
         let num_key_bits_glwe = glwe_dimension * polynomial_size;
 
         let mut task = |mut session: LargeSession| async move {
-            let mut large_preproc = DummyPreprocessing::new(seed as u64, &session);
+            let mut dummy_preproc = DummyPreprocessing::new(seed as u64, &session);
 
             //Generate the Lwe key
-            let lwe_secret_key_share = LweSecretKeyShare::<Z128, 4> {
-                data: SecureBitGenEven::gen_bits_even(
-                    num_key_bits_lwe,
-                    &mut large_preproc,
-                    &mut session,
-                )
-                .await
-                .unwrap(),
-            };
+            let lwe_secret_key_share = LweSecretKeyShare::<Z128, 4>::new_from_preprocessing(
+                LweDimension(lwe_dimension),
+                &mut dummy_preproc,
+            )
+            .unwrap();
 
             //Generate the Glwe key
-            let glwe_secret_key_share = GlweSecretKeyShare::<Z128, 4> {
-                data: SecureBitGenEven::gen_bits_even(
-                    num_key_bits_glwe,
-                    &mut large_preproc,
-                    &mut session,
-                )
-                .await
-                .unwrap(),
-                polynomial_size: PolynomialSize(polynomial_size),
-            };
+            let glwe_secret_key_share = GlweSecretKeyShare::<Z128, 4>::new_from_preprocessing(
+                num_key_bits_glwe,
+                PolynomialSize(polynomial_size),
+                &mut dummy_preproc,
+            )
+            .unwrap();
 
             //Prepare enough noise for the bk
             let t_uniform_amount =
@@ -222,7 +218,7 @@ mod tests {
             let vec_tuniform_noise = RealSecretDistributions::t_uniform(
                 t_uniform_amount,
                 TUniformBound(t_uniform_bound_glwe),
-                &mut large_preproc,
+                &mut dummy_preproc,
             )
             .unwrap()
             .iter()
@@ -245,15 +241,251 @@ mod tests {
                 &mut mpc_encryption_rng,
                 EncryptionType::Bits128,
                 &mut session,
-                &mut large_preproc,
+                &mut dummy_preproc,
             )
             .await
             .unwrap();
 
             let bk = bk_share
+                .clone()
                 .open_to_tfhers_type::<u128, _>(&session)
                 .await
                 .unwrap();
+            let bk_compressed = bk_share
+                .open_to_tfhers_seeded_type(seed, &session)
+                .await
+                .unwrap();
+            assert_eq!(bk, bk_compressed.par_decompress_into_lwe_bootstrap_key());
+
+            (
+                session.my_role(),
+                lwe_secret_key_share,
+                glwe_secret_key_share,
+                bk,
+            )
+        };
+
+        let parties = 5;
+        let threshold = 1;
+
+        //This is Async because triples are generated from dummy preprocessing
+        //Delay P1 by 1s every round
+        let delay_vec = vec![tokio::time::Duration::from_secs(1)];
+        let results = execute_protocol_large::<
+            _,
+            _,
+            ResiduePolyF4Z128,
+            { ResiduePolyF4Z128::EXTENSION_DEGREE },
+        >(
+            parties,
+            threshold,
+            None,
+            NetworkMode::Async,
+            Some(delay_vec),
+            &mut task,
+        );
+
+        let mut lwe_key_shares = HashMap::new();
+        let mut glwe_key_shares = HashMap::new();
+        let bk_ref = results[0].3.clone();
+        for (role, lwe_share, glwe_share, bk) in results.into_iter() {
+            assert_eq!(bk_ref, bk);
+
+            lwe_key_shares.insert(role, Vec::new());
+            let lwe_key_shares = lwe_key_shares.get_mut(&role).unwrap();
+            for key_share in lwe_share.data.into_iter() {
+                (*lwe_key_shares).push(key_share);
+            }
+
+            glwe_key_shares.insert(role, Vec::new());
+            let glwe_key_shares = glwe_key_shares.get_mut(&role).unwrap();
+            for key_share in glwe_share.data.into_iter() {
+                (*glwe_key_shares).push(key_share)
+            }
+        }
+        //Try and reconstruct the keys
+        let lwe_key = reconstruct_bit_vec(lwe_key_shares, num_key_bits_lwe, threshold);
+
+        let glwe_key = reconstruct_bit_vec(glwe_key_shares, num_key_bits_glwe, threshold);
+
+        //Cast both secret keys to tfhe-rs
+        let lwe_secret_key = LweSecretKeyOwned::from_container(lwe_key.clone());
+
+        let lwe_secret_key_lifted_128 =
+            LweSecretKeyOwned::from_container(lwe_key.iter().map(|b| *b as u128).collect_vec());
+
+        let glwe_secret_key = GlweSecretKeyOwned::from_container(
+            glwe_key.iter().map(|bit| *bit as u128).collect_vec(),
+            PolynomialSize(polynomial_size),
+        );
+
+        let mut rng = get_rng();
+        let mut deterministic_seeder =
+            DeterministicSeeder::<DefaultRandomGenerator>::new(seed_from_rng(&mut rng));
+        let mut enc_rng = EncryptionRandomGenerator::<DefaultRandomGenerator>::new(
+            deterministic_seeder.seed(),
+            &mut deterministic_seeder,
+        );
+
+        //We want to make sure tfhers and our bk are encryptions of the same thing
+        let bk_tfhers = allocate_and_generate_new_lwe_bootstrap_key(
+            &lwe_secret_key_lifted_128,
+            &glwe_secret_key,
+            DecompositionBaseLog(bk_base_log),
+            DecompositionLevelCount(bk_level_count),
+            DynamicDistribution::TUniform(TUniform::new(t_uniform_bound_glwe.try_into().unwrap())),
+            CoreCiphertextModulus::<u128>::new_native(),
+            &mut enc_rng,
+        );
+
+        //Decrypt all the underlying glwe ctxt and check they are all correct
+        //the BK is a list of lwe_dimension GGSW ciphertext (one GGSW ctxt for each lwe key bit)
+        assert_eq!(
+            bk_ref.deref().ggsw_ciphertext_count().0,
+            lwe_secret_key.lwe_dimension().0
+        );
+        for ((ggsw_1, ggsw_2), &input_key_bit) in bk_ref
+            .iter()
+            .zip_eq(bk_tfhers.iter())
+            .zip_eq(lwe_secret_key.as_ref())
+        {
+            //Here ggsw is a ggsw encryption of the keybit input_key_bit
+            let decrypted_ggsw_1 = decrypt_constant_ggsw_ciphertext(&glwe_secret_key, &ggsw_1);
+            let decrypted_ggsw_2 = decrypt_constant_ggsw_ciphertext(&glwe_secret_key, &ggsw_1);
+            assert_eq!(decrypted_ggsw_1.0, input_key_bit as u128);
+            assert_eq!(decrypted_ggsw_1, decrypted_ggsw_2);
+
+            //A GGSW ciphertext is a list of GgswLevelMatrix (one GgswLevelMatrix for each level)
+            for (level_idx, (ggsw_matrix_1, ggsw_matrix_2)) in
+                ggsw_1.iter().zip_eq(ggsw_2.iter()).enumerate()
+            {
+                //A GgswLevelMatrix is a list of GlweCiphertext (one GlweCiphertext for each glwe key bit-poly)
+                //Except the last row which is the plain message
+                for (glwe_ctxt_1, glwe_ctxt_2) in ggsw_matrix_1
+                    .as_glwe_list()
+                    .iter()
+                    .zip_eq(ggsw_matrix_2.as_glwe_list().iter())
+                {
+                    let mut decrypted_plaintext_list_1 =
+                        PlaintextList::new(0_u128, PlaintextCount(polynomial_size));
+                    decrypt_glwe_ciphertext(
+                        &glwe_secret_key,
+                        &glwe_ctxt_1,
+                        &mut decrypted_plaintext_list_1,
+                    );
+
+                    let mut decrypted_plaintext_list_2 =
+                        PlaintextList::new(0_u128, PlaintextCount(polynomial_size));
+                    decrypt_glwe_ciphertext(
+                        &glwe_secret_key,
+                        &glwe_ctxt_2,
+                        &mut decrypted_plaintext_list_2,
+                    );
+
+                    let decomposer = SignedDecomposer::new(
+                        DecompositionBaseLog(bk_base_log * (level_idx + 1)),
+                        DecompositionLevelCount(1),
+                    );
+
+                    decrypted_plaintext_list_1
+                        .iter_mut()
+                        .for_each(|elt| *elt.0 = decomposer.closest_representable(*elt.0));
+
+                    decrypted_plaintext_list_2
+                        .iter_mut()
+                        .for_each(|elt| *elt.0 = decomposer.closest_representable(*elt.0));
+                    assert_eq!(decrypted_plaintext_list_1, decrypted_plaintext_list_2);
+                }
+            }
+        }
+    }
+
+    #[test]
+    #[ignore] //Ignore for now, might be able to run on CI with bigger timeout though
+    fn test_lwe_bootstrap_key_from_xof() {
+        //Testing with small parameters, as NIST params take too long
+        let lwe_dimension = 32_usize;
+        let polynomial_size = 128_usize;
+        let glwe_dimension = 2_usize;
+        let t_uniform_bound_glwe = 5_usize;
+        let bk_base_log = 18_usize;
+        let bk_level_count = 3_usize;
+        let seed = 42;
+
+        let num_key_bits_lwe = lwe_dimension;
+        let num_key_bits_glwe = glwe_dimension * polynomial_size;
+
+        let mut task = |mut session: LargeSession| async move {
+            let mut dummy_preproc = DummyPreprocessing::new(seed as u64, &session);
+
+            //Generate the Lwe key
+            let lwe_secret_key_share = LweSecretKeyShare::<Z128, 4>::new_from_preprocessing(
+                LweDimension(lwe_dimension),
+                &mut dummy_preproc,
+            )
+            .unwrap();
+
+            //Generate the Glwe key
+            let glwe_secret_key_share = GlweSecretKeyShare::<Z128, 4>::new_from_preprocessing(
+                num_key_bits_glwe,
+                PolynomialSize(polynomial_size),
+                &mut dummy_preproc,
+            )
+            .unwrap();
+
+            //Prepare enough noise for the bk
+            let t_uniform_amount =
+                lwe_dimension * (glwe_dimension + 1) * bk_level_count * polynomial_size;
+            let vec_tuniform_noise = RealSecretDistributions::t_uniform(
+                t_uniform_amount,
+                TUniformBound(t_uniform_bound_glwe),
+                &mut dummy_preproc,
+            )
+            .unwrap()
+            .iter()
+            .map(|share| share.value())
+            .collect_vec();
+
+            let mut mpc_encryption_rng = MPCEncryptionRandomGenerator {
+                mask: MPCMaskRandomGenerator::<SoftwareRandomGenerator>::new_from_seed(
+                    XofSeed::new_u128(seed, [1; 8]),
+                ),
+                noise: MPCNoiseRandomGenerator {
+                    vec: vec_tuniform_noise,
+                },
+            };
+
+            //Generate the bk
+            let bk_share = allocate_and_generate_lwe_bootstrap_key(
+                &lwe_secret_key_share,
+                &glwe_secret_key_share,
+                DecompositionBaseLog(bk_base_log),
+                DecompositionLevelCount(bk_level_count),
+                &mut mpc_encryption_rng,
+                EncryptionType::Bits128,
+                &mut session,
+                &mut dummy_preproc,
+            )
+            .await
+            .unwrap();
+
+            let bk = bk_share
+                .clone()
+                .open_to_tfhers_type::<u128, _>(&session)
+                .await
+                .unwrap();
+            let bk_compressed = bk_share
+                .open_to_tfhers_seeded_type(seed, &session)
+                .await
+                .unwrap();
+
+            assert_eq!(
+                bk,
+                par_decompress_into_lwe_bootstrap_key_generated_from_xof::<
+                    _,
+                    SoftwareRandomGenerator,
+                >(bk_compressed.clone(), [1; 8])
+            );
 
             (
                 session.my_role(),
