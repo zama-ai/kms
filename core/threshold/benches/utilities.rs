dc5349aa25562591cd0f7236e9a86d4e34b132f6
Titouan Tanguy <titouan.tanguy@zama.ai>
2025-09-04 17:28:21 +0200

    chore: rework memory measurement

--- core/threshold/benches/utilities.rs
+++ core/threshold/benches/utilities.rs
@@ -1,6 +1,4 @@
 #[cfg(feature = "measure_memory")]
-use criterion::profiler::Profiler;
-#[cfg(feature = "measure_memory")]
 use threshold_fhe::allocator::MEM_ALLOCATOR;
 use threshold_fhe::execution::tfhe_internals::parameters::DKGParams;
 use threshold_fhe::execution::tfhe_internals::parameters::{
@@ -8,36 +6,9 @@ use threshold_fhe::execution::tfhe_internals::parameters::{
     NIST_PARAMS_P8_SNS_LWE,
 };
 
-/// Best workaround to using criterion to measure memory seems to be using the Profiler.
-/// We can then get memory reporting using
-/// cargo bench -- --profile-time <num_seconds>
-/// inspired by this discussion:
-/// https://github.com/bheisler/criterion.rs/issues/97
-///
-/// Couple of issues though:
-/// - we are most likely also measuring criterion's overhead
-/// - we are only measuring memory allocated on the heap
-#[cfg(feature = "measure_memory")]
-pub struct MemoryProfiler;
-
-#[cfg(feature = "measure_memory")]
-impl Profiler for MemoryProfiler {
-    fn start_profiling(&mut self, _: &str, _: &std::path::Path) {
-        MEM_ALLOCATOR.get().unwrap().reset_peak_usage();
-        println!(
-            "\nABOUT TO START MEMORY PROFILER, RESET PEAK USAGE TO CURRENT {}",
-            MEM_ALLOCATOR.get().unwrap().peak_usage()
-        );
-    }
-
-    fn stop_profiling(&mut self, _: &str, _: &std::path::Path) {
-        let size = MEM_ALLOCATOR.get().unwrap().peak_usage();
-        println!("Profiling finished. Allocated {size} B");
-    }
-}
-
 #[cfg(feature = "measure_memory")]
 fn print_memory_usage(bench_name: String, results: Vec<usize>) {
+    println!("ALL RESULTS: {:?}", results);
     let num_runs = results.len();
     // Compute mean and std deviation of the results
     let mean = results.iter().sum::<usize>() as f64 / num_runs as f64;
