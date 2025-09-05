f0c6a3561a0f6c24151178aa9448c4c20e78f81a
Titouan Tanguy <titouan.tanguy@zama.ai>
2025-09-05 11:35:22 +0200

    chore: add scripts

---
+++ NIST_scripts/non-threshold-bench.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is used to run the non-threshold benchmarks for the NIST submission.
+# In particular, it runs the benchmarks located in core/threshold/benches/non-threshold.
+# And parses the results into a format suitable for the NIST submission.
+
+
+# Assumes the build.sh script was used
+TARGET_DIR="$HOME/kms/core/threshold"
+OUTPUT_FILE="$TARGET_DIR/bench_results.json"
+
+touch $OUTPUT_FILE
+
+# Run the latency benchmarks
+cargo-criterion --bench non-threshold_keygen --message-format json >> $OUTPUT_FILE
+cargo-criterion --bench non-threshold_basic-ops --message-format json >> $OUTPUT_FILE
+cargo-criterion --bench non-threshold_erc20 --message-format json >> $OUTPUT_FILE
+
+# Run the memory benchmarks
+cargo --bench non-threshold_keygen --features=measure_memory >> $OUTPUT_FILE
+cargo --bench non-threshold_basic-ops --features=measure_memory >> $OUTPUT_FILE
+cargo --bench non-threshold_erc20 --features=measure_memory >> $OUTPUT_FILE
