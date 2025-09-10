4b2526e97d7bbda64ac9a1bae2b96cbe95de5857
Titouan Tanguy <titouan.tanguy@zama.ai>
2025-09-10 15:49:29 +0200

    chore: try serializing some coinflip tests

--- core/threshold/src/execution/large_execution/coinflip.rs
+++ core/threshold/src/execution/large_execution/coinflip.rs
@@ -257,10 +257,11 @@ pub(crate) mod tests {
 
     // Rounds: We expect 3+1+t+1 rounds on the happy path
     #[rstest]
-    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+    #[tokio::test]
     #[case(TestingParameters::init_honest(4, 1, Some(6)))]
     #[case(TestingParameters::init_honest(7, 2, Some(7)))]
     #[case(TestingParameters::init_honest(10, 3, Some(8)))]
+    #[serial_test::serial(coinflip_1)]
     async fn test_coinflip_honest_z128(#[case] params: TestingParameters) {
         let malicious_coinflip = SecureCoinflip::default();
         test_coinflip_strategies::<ResiduePolyF4Z64, { ResiduePolyF4Z64::EXTENSION_DEGREE }, _>(
@@ -279,7 +280,7 @@ pub(crate) mod tests {
     //No matter the strategy we expect all honest parties to output the same thing
     //We also specify whether we expect the cheating strategy to be detected, if so we check we do detect the cheaters
     #[rstest]
-    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+    #[tokio::test]
     #[case(TestingParameters::init(4, 1, &[1], &[], &[], false, None), SecureVss::default())]
     #[case(TestingParameters::init(4, 1, &[1], &[], &[], true, None), DroppingVssAfterR1::default())]
     #[case(TestingParameters::init(4, 1, &[1], &[], &[], false, None), DroppingVssAfterR2::new(&SyncReliableBroadcast::default()))]
@@ -288,6 +289,7 @@ pub(crate) mod tests {
     #[case(TestingParameters::init(7, 2, &[1,3], &[0,2], &[], false, None), MaliciousVssR1::new(&SyncReliableBroadcast::default(),&params.roles_to_lie_to))]
     #[case(TestingParameters::init(7, 2, &[1,3], &[0,2,4,6], &[], true, None), MaliciousVssR1::new(&SyncReliableBroadcast::default(),&params.roles_to_lie_to))]
     #[cfg(feature = "slow_tests")]
+    #[serial_test::serial(coinflip_2)]
     async fn test_coinflip_dropout<V: Vss + 'static>(
         #[case] params: TestingParameters,
         #[case] malicious_vss: V,
@@ -311,7 +313,7 @@ pub(crate) mod tests {
     //No matter the strategy, we expect all honest parties to end up with the same output
     //We also specify whether we expect the cheating strategy to be detected, if so we check we do detect the cheaters
     #[rstest]
-    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+    #[tokio::test]
     #[case(TestingParameters::init(4, 1, &[1], &[], &[], true, None), DroppingVssFromStart::default(),SecureRobustOpen::default())]
     #[case(TestingParameters::init(4, 1, &[1], &[], &[], true, None), DroppingVssAfterR1::default(),SecureRobustOpen::default())]
     #[case(TestingParameters::init(4, 1, &[1], &[], &[], false, None), DroppingVssAfterR2::new(&SyncReliableBroadcast::default()),SecureRobustOpen::default())]
@@ -319,6 +321,7 @@ pub(crate) mod tests {
     #[case(TestingParameters::init(4, 1, &[1], &[0,2], &[], true, None), MaliciousVssR1::new(&SyncReliableBroadcast::default(),&params.roles_to_lie_to),SecureRobustOpen::default())]
     #[case(TestingParameters::init(7, 2, &[1,3], &[0,2], &[], false, None), MaliciousVssR1::new(&SyncReliableBroadcast::default(),&params.roles_to_lie_to),SecureRobustOpen::default())]
     #[case(TestingParameters::init(7, 2, &[1,3], &[0,2,4,6], &[], true, None), MaliciousVssR1::new(&SyncReliableBroadcast::default(),&params.roles_to_lie_to),SecureRobustOpen::default())]
+    #[serial_test::serial(coinflip_3)]
     #[cfg(feature = "slow_tests")]
     async fn test_coinflip_malicious_vss<V: Vss + 'static, RO: RobustOpen + 'static>(
         #[case] params: TestingParameters,
@@ -345,7 +348,7 @@ pub(crate) mod tests {
     //Test malicious coinflip with all kinds of strategies for VSS (honest and malicious)
     //Again, we always expect the honest parties to agree on the output
     #[rstest]
-    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+    #[tokio::test]
     #[case(TestingParameters::init(4, 1, &[1], &[], &[], false, None), SecureVss::default(),SecureRobustOpen::default())]
     #[case(TestingParameters::init(4, 1, &[1], &[], &[], true, None), DroppingVssAfterR1::default(),SecureRobustOpen::default())]
     #[case(TestingParameters::init(4, 1, &[1], &[], &[], false, None), DroppingVssAfterR2::new(&SyncReliableBroadcast::default()),SecureRobustOpen::default())]
@@ -353,6 +356,7 @@ pub(crate) mod tests {
     #[case(TestingParameters::init(4, 1, &[1], &[0,2], &[], true, None), MaliciousVssR1::new(&SyncReliableBroadcast::default(),&params.roles_to_lie_to),SecureRobustOpen::default())]
     #[case(TestingParameters::init(7, 2, &[1,3], &[0,2], &[], false, None), MaliciousVssR1::new(&SyncReliableBroadcast::default(),&params.roles_to_lie_to),SecureRobustOpen::default())]
     #[case(TestingParameters::init(7, 2, &[1,3], &[0,2,4,6], &[], true, None), MaliciousVssR1::new(&SyncReliableBroadcast::default(),&params.roles_to_lie_to),SecureRobustOpen::default())]
+    #[serial_test::serial(coinflip_4)]
     #[cfg(feature = "slow_tests")]
     async fn test_malicious_coinflip_malicious_vss<V: Vss + 'static, RO: RobustOpen + 'static>(
         #[case] params: TestingParameters,
