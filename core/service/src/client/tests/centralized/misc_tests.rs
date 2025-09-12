566719771804bc4c91d78f9bbc3746b2eb27d89a
Tore Frederiksen <tore.frederiksen@zama.ai>
2025-09-12 10:50:49 +0200

    refactor: moved tests from misc_tests

--- core/service/src/client/tests/centralized/misc_tests.rs
+++ core/service/src/client/tests/centralized/misc_tests.rs
@@ -1,49 +1,28 @@
-#[cfg(feature = "slow_tests")]
-use crate::client::client_wasm::Client;
 use crate::client::test_tools::{get_health_client, get_status};
 use crate::client::tests::common::TIME_TO_SLEEP_MS;
 use crate::consts::TEST_CENTRAL_KEY_ID;
 use crate::consts::TEST_PARAM;
-#[cfg(feature = "slow_tests")]
-use crate::dummy_domain;
-#[cfg(feature = "slow_tests")]
-use crate::engine::centralized::central_kms::tests::get_default_keys;
 use crate::engine::centralized::central_kms::RealCentralizedKms;
-#[cfg(feature = "slow_tests")]
-use crate::util::rate_limiter::RateLimiterConfig;
 use crate::vault::storage::file::FileStorage;
-#[cfg(feature = "slow_tests")]
-use kms_grpc::kms::v1::TypedCiphertext;
 use kms_grpc::kms_service::v1::core_service_endpoint_server::CoreServiceEndpointServer;
 use serial_test::serial;
 use std::collections::HashMap;
-#[cfg(feature = "slow_tests")]
-use tfhe::FheTypes;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "slow_tests")] {
+        use tfhe::FheTypes;
+        use kms_grpc::kms::v1::TypedCiphertext;
+        use crate::engine::base::derive_request_id;
+        use crate::util::rate_limiter::RateLimiterConfig;
+        use crate::engine::centralized::central_kms::tests::get_default_keys;
+        use crate::dummy_domain;
+        use crate::client::client_wasm::Client;
+        use kms_grpc::kms::v1::Empty;
+    }
+}
 use tonic::server::NamedService;
 use tonic_health::pb::health_check_response::ServingStatus;
 use tonic_health::pb::HealthCheckRequest;
-cfg_if::cfg_if! {
-   if #[cfg(feature = "insecure")] {
-        use crate::{
-            cryptography::internal_crypto_types::WrappedDKGParams, engine::base::derive_request_id,
-            util::key_setup::test_tools::purge,
-        };
-        use kms_grpc::{kms::v1::FheParameter, RequestId};
-        use crate::util::key_setup::test_tools::EncryptionConfig;
-        use kms_grpc::kms::v1::Empty;
-        use crate::{
-            client::tests::centralized::{
-                key_gen_tests::key_gen_centralized, public_decryption_tests::decryption_centralized,
-            },
-            util::key_setup::test_tools::TestingPlaintext,
-            vault::storage::{delete_all_at_request_id, StorageType},
-        };
-        use kms_grpc::rpc_types::{PrivDataType};
-        use crate::{vault::storage::make_storage};
-        use crate::util::key_setup::test_tools::purge_backup;
-        use crate::{client::tests::centralized::crs_gen_tests::crs_gen_centralized, vault::storage::StorageReader};
-   }
-}
+
 /// Check that the centralized health service is serving as soons as boot is completed.
 #[tokio::test]
 #[serial]
@@ -223,136 +202,4 @@ async fn test_largecipher() {
         .contains("finished with an error"));
     tracing::info!("aborting");
     kms_server.assert_shutdown().await;
-}
-
-#[cfg(feature = "insecure")]
-#[tokio::test(flavor = "multi_thread")]
-#[serial]
-async fn default_insecure_central_dkg_backup() {
-    let param = FheParameter::Default;
-    let dkg_param: WrappedDKGParams = param.into();
-    let key_id_1 = derive_request_id("default_insecure_central_dkg_backup-1").unwrap();
-    let key_id_2 = derive_request_id("default_insecure_central_dkg_backup-2").unwrap();
-    // Delete potentially old data
-    purge(None, None, None, &key_id_1, 1).await;
-    purge(None, None, None, &key_id_2, 1).await;
-    purge_backup(None, 1).await;
-    key_gen_centralized(&key_id_1, param, None, None).await;
-    key_gen_centralized(&key_id_2, param, None, None).await;
-    // Generated key, delete private storage
-    let mut priv_storage: FileStorage = FileStorage::new(None, StorageType::PRIV, None).unwrap();
-    delete_all_at_request_id(&mut priv_storage, &key_id_1).await;
-    delete_all_at_request_id(&mut priv_storage, &key_id_2).await;
-
-    // Now try to restore both keys
-    let (kms_server, mut kms_client, internal_client) =
-        crate::client::test_tools::centralized_handles(&dkg_param, None).await;
-
-    let req = Empty {};
-    // send query
-    match kms_client
-        .restore_from_backup(tonic::Request::new(req))
-        .await
-    {
-        Ok(res) => tracing::info!("Backup restore response: {res:?}"),
-        Err(e) => {
-            panic!("Error while restoring: {e}");
-        }
-    }
-
-    drop(kms_server);
-    drop(kms_client);
-    drop(internal_client);
-    // Sleep to ensure the servers are properly shut down
-    tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-    decryption_centralized(
-        &dkg_param.get_params_without_sns(),
-        &key_id_1,
-        vec![TestingPlaintext::U8(u8::MAX)],
-        EncryptionConfig {
-            compression: false,
-            precompute_sns: true,
-        },
-        1,
-    )
-    .await;
-}
-
-#[cfg(feature = "insecure")]
-#[tokio::test(flavor = "multi_thread")]
-#[serial]
-async fn default_insecure_central_autobackup_after_deletion() {
-    let param = FheParameter::Default;
-    let dkg_param: WrappedDKGParams = param.into();
-    let key_id = derive_request_id("default_insecure_autobackup_after_deletion").unwrap();
-    // Delete potentially old data
-    purge(None, None, None, &key_id, 1).await;
-    purge_backup(None, 1).await;
-    key_gen_centralized(&key_id, param, None, None).await;
-    // Sleep to ensure the servers are properly shut down
-    tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-    // Start the servers again
-    let (_kms_server, _kms_client, _internal_client) =
-        crate::client::test_tools::centralized_handles(&dkg_param, None).await;
-    // Check the storage
-    let backup_storage = make_storage(None, StorageType::BACKUP, None, None, None).unwrap();
-    // Validate that the backup is constructed again
-    assert!(backup_storage
-        .data_exists(&key_id, &PrivDataType::FhePrivateKey.to_string())
-        .await
-        .unwrap());
-}
-
-#[cfg(feature = "insecure")]
-#[tokio::test(flavor = "multi_thread")]
-#[serial]
-async fn default_insecure_central_crs_backup() {
-    let param = FheParameter::Default;
-    let dkg_param: WrappedDKGParams = param.into();
-    let req_id: RequestId =
-        derive_request_id(&format!("default_insecure_central_crs_backup_{param:?}",)).unwrap();
-    purge(None, None, None, &req_id, 1).await;
-    purge_backup(None, 1).await;
-    crs_gen_centralized(&req_id, param, true).await;
-
-    // Generated crs, delete it from private storage
-    let mut priv_storage: FileStorage = FileStorage::new(None, StorageType::PRIV, None).unwrap();
-    delete_all_at_request_id(&mut priv_storage, &req_id).await;
-    // Check that is has been removed
-    assert!(!priv_storage
-        .data_exists(&req_id, &PrivDataType::CrsInfo.to_string())
-        .await
-        .unwrap());
-
-    // It will get auto-backed up at boot
-    let (_kms_server, mut kms_client, _internal_client) =
-        crate::client::test_tools::centralized_handles(&dkg_param, None).await;
-
-    let req = Empty {};
-    // Now try to restore the crs
-    let query_res = kms_client
-        .restore_from_backup(tonic::Request::new(req))
-        .await;
-    match query_res {
-        Ok(resp) => {
-            tracing::info!("Backup restore response: {resp:?}");
-        }
-        Err(e) => {
-            panic!("Error while restoring: {e}");
-        }
-    }
-
-    let backup_storage: FileStorage = FileStorage::new(None, StorageType::BACKUP, None).unwrap();
-    // Check the back up is still there
-    assert!(backup_storage
-        .data_exists(&req_id, &PrivDataType::CrsInfo.to_string())
-        .await
-        .unwrap());
-    // Check that the file has been restored
-    let priv_storage: FileStorage = FileStorage::new(None, StorageType::PRIV, None).unwrap();
-    // Check the back up is still there
-    assert!(priv_storage
-        .data_exists(&req_id, &PrivDataType::CrsInfo.to_string())
-        .await
-        .unwrap());
 }
--- core/service/src/client/tests/threshold/mod.rs
+++ core/service/src/client/tests/threshold/mod.rs
@@ -7,4 +7,6 @@ mod misc_tests;
 #[cfg(feature = "slow_tests")]
 mod nightly_tests;
 mod public_decryption_tests;
+#[cfg(feature = "insecure")]
+mod restore_from_backup_tests;
 mod user_decryption_tests;
