34775d1b11671d7279b82e3a9bdad8fae7f64038
Titouan Tanguy <titouan.tanguy@zama.ai>
2025-10-10 17:58:32 +0200

    chore: linter

--- core/service/src/client/tests/threshold/mod.rs
+++ core/service/src/client/tests/threshold/mod.rs
@@ -7,6 +7,7 @@ mod misc_tests;
 #[cfg(feature = "slow_tests")]
 mod nightly_tests;
 mod public_decryption_tests;
+#[cfg(feature = "slow_tests")]
 mod reshare_tests;
 #[cfg(feature = "insecure")]
 mod restore_from_backup_tests;
