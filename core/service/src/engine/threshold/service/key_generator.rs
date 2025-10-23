85897fbe8759d416efff29f70337941030c6921d
Kelong Cong <kc1212@users.noreply.github.com>
2025-10-23 14:06:38 +0200

    chore: move delay into background task

--- core/service/src/engine/threshold/service/key_generator.rs
+++ core/service/src/engine/threshold/service/key_generator.rs
@@ -226,6 +226,27 @@ impl<
         let context_id = context_id.unwrap_or(*DEFAULT_MPC_CONTEXT);
         let session_preparer = self.session_preparer_getter.get(&context_id).await?;
 
+        // TODO(zama-ai/kms-internal/issues/2802)
+        // Wait for the round timeout in case I've not finished with preprocessing.
+        // Note that the round timeout should be the one in the synchronous setting,
+        // that's why we have to get it from the config and not the session,
+        // as the async session used in keygen will set the timeout to one year.
+        //
+        // This is not the complete solution for solving #2802, but a temporary workaround.
+        // So remove this block of code below when the proper fix is done.
+        let keygen_delay = {
+            let conf = session_preparer
+                .get_core_to_core_config()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Internal,
+                        format!("Empty session preparer: {e}"),
+                    )
+                })?;
+            conf.get_network_timeout()
+        };
+
         //Retrieve the right metric tag
         let op_tag = match (
             &preproc_handle_w_mode,
@@ -297,6 +318,10 @@ impl<
         let req_id_clone = req_id;
         let opt_compression_key_id = internal_keyset_config.get_compression_id()?;
         let keygen_background = async move {
+            // TODO(zama-ai/kms-internal/issues/2802)
+            // Remove this sleep once the issue above is resolved.
+            tokio::time::sleep(keygen_delay).await;
+
             match internal_keyset_config.keyset_config() {
                 ddec_keyset_config::KeySetConfig::Standard(inner_config) => {
                     Self::key_gen_background(
@@ -422,50 +447,6 @@ impl<
         // because resource exhaustion can be recovered by sending the exact same request
         // but the errors above cannot be tried again.
         let permit = self.rate_limiter.start_keygen().await?;
-
-        // TODO(zama-ai/kms-internal/issues/2802)
-        // Wait for the round timeout in case I've not finished with preprocessing.
-        // Note that the round timeout should be the one in the synchronous setting,
-        // that's why we have to get it from the config and not the session,
-        // as the async session used in keygen will set the timeout to one year.
-        //
-        // This is not the complete solution for solving #2802, but a temporary workaround.
-        // So remove this block of code below when the proper fix is done.
-        {
-            let context_id = inner
-                .context_id
-                .as_ref()
-                .map(|id| id.try_into())
-                .transpose()
-                .map_err(|e| {
-                    tonic::Status::new(
-                        tonic::Code::InvalidArgument,
-                        format!("invalid context id: {e}"),
-                    )
-                })?
-                .unwrap_or(*DEFAULT_MPC_CONTEXT);
-            let session_preparer = self
-                .session_preparer_getter
-                .get(&context_id)
-                .await
-                .map_err(|e| {
-                    tonic::Status::new(
-                        tonic::Code::InvalidArgument,
-                        format!("Missing context: {e}"),
-                    )
-                })?;
-            let conf = session_preparer
-                .get_core_to_core_config()
-                .await
-                .map_err(|e| {
-                    tonic::Status::new(
-                        tonic::Code::Internal,
-                        format!("Empty session preparer: {e}"),
-                    )
-                })?;
-            let network_timeout = conf.get_network_timeout();
-            tokio::time::sleep(network_timeout).await;
-        }
 
         // TODO(zama-ai/kms-internal/issues/2722)
         // consider moving this block of code further down the stack,
