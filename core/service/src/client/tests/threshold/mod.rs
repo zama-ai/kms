ca6cffca1fd6e171cc3aaa5295e4b9a7adccbdf7
Maksym Surzhynskyi <maksym.surzhynskyi@zama.ai>
2025-11-18 05:13:43 +0200

    refactor: major tests migration complete

--- core/service/src/client/tests/threshold/mod.rs
+++ core/service/src/client/tests/threshold/mod.rs
@@ -3,7 +3,11 @@ mod crs_gen_tests;
 mod custodian_backup_tests;
 mod custodian_context_tests;
 mod key_gen_tests;
+#[cfg(any(test, feature = "testing"))]
+mod key_gen_tests_isolated;
 mod misc_tests;
+#[cfg(any(test, feature = "testing"))]
+mod misc_tests_isolated;
 mod mpc_context_tests;
 #[cfg(feature = "slow_tests")]
 mod nightly_tests;
@@ -12,4 +16,6 @@ mod public_decryption_tests;
 mod reshare_tests;
 #[cfg(feature = "slow_tests")]
 mod restore_from_backup_tests;
+#[cfg(any(test, feature = "testing"))]
+mod restore_from_backup_tests_isolated;
 mod user_decryption_tests;
