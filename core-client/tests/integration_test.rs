434c80274a387e04a13e05cf5b7c47f5922379b3
Tore Frederiksen <tore.frederiksen@zama.ai>
2026-01-23 15:41:57 +0100

    fix: test

--- core-client/tests/integration_test.rs
+++ core-client/tests/integration_test.rs
@@ -1460,9 +1460,7 @@ async fn test_threshold_mpc_context_switch_6(ctx: &DockerComposeThresholdTestNoI
         )
         .await
         .unwrap_err();
-        assert!(err
-            .to_string()
-            .contains(&format!("Context {context_id} not found")));
+        assert!(err.to_string().contains(&format!("NotFound")));
     }
 }
 
