zama-ai/kms

commit 08bd9acf617279b622f347969b240a04bacbd34b

fix: remove use of `zip` (#2550)
* refactor: initial removal of zip

* fix: issue with check

* fix: issue with check

* fix: bug in zip in pack_residue_poly

* refactor: degree handling

* refactor: incremental

* fix: incremental

* fix: incremental

* fix: incremental. Handled more of threshold

* refactor: incremental zip removal

* refactor: incremental

* fix: logging details

* fix: length check in ceremony

* refactor: panultimate incremental

* fix: handle constraints in cyclotomic

* refactor: ntt

* fix: handle padding issue

* refactor: experimental revert

* refactor: experimental revert

* fix: changed logic for fill shares

* docs: Update core/threshold/src/algebra/galois_rings/common.rs

Co-authored-by: Daniel Demmler <daniel.demmler@zama.ai>

* docs: Update core/threshold/src/algebra/poly.rs

Co-authored-by: Daniel Demmler <daniel.demmler@zama.ai>

* docs: Update core/threshold/src/execution/large_execution/double_sharing.rs

Co-authored-by: Daniel Demmler <daniel.demmler@zama.ai>

* refactor: minor review comments

* fix dylint

* fix: linter

* refactor: changed things to panic according to Titouan's request

* fix: ensure panic in left side of degree evaluation

* refactor: changed to zip according to review

---------

Co-authored-by: Daniel Demmler <daniel.demmler@zama.ai>

core/threshold/src/execution/online/triple.rs
@@ -76,7 +76,7 @@ pub async fn mult_list<Z: Ring + ErrorCorrect, Ses: BaseSessionHandles>(
     }
     let mut to_open = Vec::with_capacity(2 * amount);
     // Compute the shares of epsilon and rho and merge them together into a single list
-    for ((cur_x, cur_y), cur_trip) in x_vec.iter().zip(y_vec).zip(&triples) {
+    for ((cur_x, cur_y), cur_trip) in x_vec.iter().zip_eq(y_vec).zip_eq(&triples) {
         if cur_x.owner() != cur_y.owner()
             || cur_trip.a.owner() != cur_x.owner()
             || cur_trip.b.owner() != cur_x.owner()

core/threshold/src/execution/sharing/shamir.rs
@@ -1,9 +1,11 @@
 use super::share::Share;
 use crate::algebra::poly::Poly;
-use crate::algebra::structure_traits::Ring;
-use crate::algebra::structure_traits::{ErrorCorrect, RingEmbed};
+use crate::algebra::structure_traits::{ErrorCorrect, Ring, RingEmbed};
+use crate::error::error_handler::anyhow_error_and_log;
 use crate::error::error_handler::anyhow_error_and_warn_log;
 use crate::execution::runtime::party::Role;
+use itertools::EitherOrBoth::{Both, Left, Right};
+use itertools::Itertools;
 use rand::{CryptoRng, Rng};
 use std::ops::{Add, Mul, Sub};
 
@@ -57,13 +59,18 @@ impl<Z: Ring> ShamirSharings<Z> {
 impl<Z: Ring> Add<ShamirSharings<Z>> for ShamirSharings<Z> {
     type Output = ShamirSharings<Z>;
     fn add(self, rhs: ShamirSharings<Z>) -> Self::Output {
+        let pair = self.shares.into_iter().zip_longest(rhs.shares);
         ShamirSharings {
-            shares: self
-                .shares
-                .into_iter()
-                .zip(rhs.shares)
-                .map(|(a, b)| a + b)
-                .collect(),
+            shares: pair
+                .map(|cur| {
+                    match cur {
+                        Both(a, b) => a + b,
+                        // If only one side has a share, we interpret the other side as zero
+                        Left(a) => a,
+                        Right(b) => b,
+                    }
+                })
+                .collect_vec(),
         }
     }
 }
@@ -71,16 +78,21 @@ impl<Z: Ring> Add<ShamirSharings<Z>> for ShamirSharings<Z> {
 impl<Z: Ring> Add<&ShamirSharings<Z>> for &ShamirSharings<Z> {
     type Output = ShamirSharings<Z>;
     fn add(self, rhs: &ShamirSharings<Z>) -> Self::Output {
+        let pair = self.shares.iter().zip_longest(rhs.shares.iter());
         ShamirSharings {
-            shares: self
-                .shares
-                .iter()
-                .zip(rhs.shares.iter())
-                .map(|(a, b)| {
-                    assert_eq!(a.owner(), b.owner());
-                    Share::new(a.owner(), a.value() + b.value())
+            shares: pair
+                .map(|cur| {
+                    match cur {
+                        Both(a, b) => {
+                            assert_eq!(a.owner(), b.owner());
+                            Share::new(a.owner(), a.value() + b.value())
+                        }
+                        // If only one side has a share, we interpret the other side as zero
+                        Left(a) => Share::new(a.owner(), a.value()),
+                        Right(b) => Share::new(b.owner(), b.value()),
+                    }
                 })
-                .collect(),
+                .collect_vec(),
         }
     }
 }
@@ -88,16 +100,21 @@ impl<Z: Ring> Add<&ShamirSharings<Z>> for &ShamirSharings<Z> {
 impl<Z: Ring> Sub<&ShamirSharings<Z>> for &ShamirSharings<Z> {
     type Output = ShamirSharings<Z>;
     fn sub(self, rhs: &ShamirSharings<Z>) -> Self::Output {
+        let pair = self.shares.iter().zip_longest(rhs.shares.iter());
         ShamirSharings {
-            shares: self
-                .shares
-                .iter()
-                .zip(rhs.shares.iter())
-                .map(|(a, b)| {
-                    assert_eq!(a.owner(), b.owner());
-                    Share::new(a.owner(), a.value() - b.value())
+            shares: pair
+                .map(|cur| {
+                    match cur {
+                        Both(a, b) => {
+                            assert_eq!(a.owner(), b.owner());
+                            Share::new(a.owner(), a.value() - b.value())
+                        }
+                        // If only one side has a share, we interpret the other side as zero
+                        Left(a) => Share::new(a.owner(), a.value()),
+                        Right(b) => Share::new(b.owner(), b.value()),
+                    }
                 })
-                .collect(),
+                .collect_vec(),
         }
     }
 }
@@ -196,8 +213,9 @@ where
     }
 }
 
-/// Maps `values` into [ShamirSharings]s by appending these to `sharings`.
-/// Furthermore, ensure that at least `num_values` shares are added to `sharings`.
+/// Maps `values` into [ShamirSharings]s by pairwise adding each of these to each of the `sharings`.
+/// Furthermore, adds a zero-share for each share in `sharings` if there is not supplied enough `values`.
+/// This in turn means that there will be `num_values` shares in `sharings`.
 /// The function is useful to ensure that an indexable vector of Shamir shares exist.
 pub fn fill_indexed_shares<Z: Ring>(
     sharings: &mut [ShamirSharings<Z>],
@@ -205,24 +223,46 @@ pub fn fill_indexed_shares<Z: Ring>(
     num_values: usize,
     party_id: Role,
 ) -> anyhow::Result<()> {
+    if sharings.len() != num_values {
+        return Err(anyhow_error_and_log(format!(
+            "Number of sharings {} is not the expected amount {} from party {}",
+            sharings.len(),
+            num_values,
+            party_id
+        )));
+    }
+    if sharings.len() < values.len() {
+        return Err(anyhow_error_and_log(format!(
+            "Number of sharings {} is not {} as expected from party {}.",
+            sharings.len(),
+            values.len(),
+            party_id
+        )));
+    }
+    let sharing_len = sharings.len();
     let values_len = values.len();
-
-    values
-        .into_iter()
-        .zip(sharings.iter_mut())
-        .for_each(|(v, sharing)| sharing.add_share(Share::new(party_id, v)));
-
-    // fill up to num_values with 0s if not enough values were provided
-    if values_len < num_values {
-        tracing::warn!(
-            "Received {} shares from party {} but expected {}. Filling with 0s",
-            values_len,
-            party_id,
-            num_values
-        );
-        for sharing in sharings.iter_mut().skip(values_len) {
-            // We simply log the error and continue triyng to fill with zeros
-            sharing.add_share(Share::new(party_id, Z::ZERO))
+    let pair = values.into_iter().zip_longest(sharings.iter_mut());
+    for cur in pair {
+        match cur {
+            Both(v, sharing) => {
+                sharing.add_share(Share::new(party_id, v));
+            }
+            Left(_v) => {
+                // If a share is missing, we panic since we already checked the lengths in the start of the method
+                panic!(
+                    "There are {sharing_len} shares, but {values_len} values. There should not be more values than shares from party {party_id}." 
+                );
+            }
+            Right(sharing) => {
+                // If a value is missing, then add a zero-share in liu of the value
+                tracing::warn!(
+                    "Received {} shares from {} but expected {}. Filling with 0s",
+                    values_len,
+                    party_id,
+                    num_values
+                );
+                sharing.add_share(Share::new(party_id, Z::ZERO));
+            }
         }
     }
     Ok(())
@@ -633,7 +673,8 @@ mod tests {
                     )
                     .unwrap();
 
-                    for (share, all_share) in sharings.shares.into_iter().zip(all_shares.iter_mut())
+                    for (share, all_share) in
+                        sharings.shares.into_iter().zip_eq(all_shares.iter_mut())
                     {
                         let share_buf_bincode = bc2wrap::serialize(&share.value()).unwrap();
                         let share_buf = share.value().to_byte_vec();

commit ce666bb74875df51f099c69a86fe463eb6965ced

chore: Malicious strategies + fix error introduced in #2550

core/service/src/client/user_decryption_wasm.rs
@@ -483,7 +483,7 @@ impl Client {
                     cur_blocks,
                     num_values,
                     Role::indexed_from_one(payload.party_id as usize),
-                )?;
+                );
             }
             let sharings = opt_sharings.unwrap();
             // TODO: in general this is not true, degree isn't a perfect proxy for num_parties
@@ -676,7 +676,7 @@ impl Client {
                             cur_blocks,
                             num_shares,
                             Role::indexed_from_one(cur_resp.party_id as usize),
-                        )?;
+                        );
                     }
                     Err(e) => {
                         tracing::warn!(

core/threshold/src/execution/online/bit_lift.rs
@@ -1,12 +1,11 @@
 use std::sync::Arc;
 
-use itertools::Itertools;
 use tonic::async_trait;
 
 use crate::{
     algebra::{
         base_ring::{Z128, Z64},
-        galois_rings::common::{pack_residue_poly, Monomials, ResiduePoly},
+        galois_rings::common::{Monomials, ResiduePoly},
         structure_traits::{ErrorCorrect, FromU128},
     },
     execution::{
@@ -18,10 +17,11 @@ use crate::{
         runtime::sessions::base_session::BaseSessionHandles,
         sharing::share::Share,
     },
+    ProtocolDescription,
 };
 
 #[async_trait]
-pub trait BitLift {
+pub trait BitLift: Send + Sync + Clone + ProtocolDescription {
     /// Lifts a vector of bits shared over a galois extension of Z64 into a vector of bits shared over a galois estension of Z128
     /// # Arguments
     /// * `secret_bit_vector` - Vector of bits shared over a galois extension of Z64
@@ -48,7 +48,15 @@ pub trait BitLift {
         ResiduePoly<Z128, EXTENSION_DEGREE>: ErrorCorrect;
 }
 
+#[derive(Default, Clone)]
 pub struct SecureBitLift;
+
+impl ProtocolDescription for SecureBitLift {
+    fn protocol_desc(depth: usize) -> String {
+        let indent = Self::INDENT_STRING.repeat(depth);
+        format!("{indent}-SecureBitLift")
+    }
+}
 
 #[async_trait]
 impl BitLift for SecureBitLift {
@@ -74,64 +82,34 @@ impl BitLift for SecureBitLift {
 
         // Copy of the randoms bits, except modswitched to Z64 (reminiscent of "dabits")
         // note that they are still the same bits as in Z128
-        let random_bits_z64 = random_bits_z128
-            .iter()
-            .map(|b| Share::new(b.owner(), b.value().to_residuepoly64()))
-            .collect::<Vec<Share<ResiduePoly<Z64, EXTENSION_DEGREE>>>>();
-
-        // Pack bits and inputs into every coef of the extension ring
-        let packed_secret_bits = Arc::new(
-            pack_residue_poly(
-                &(secret_bit_vector
-                    .into_iter()
-                    .map(|s| s.value())
-                    .collect::<Vec<_>>()),
-            )
-            .into_iter()
-            .map(|x| Share::new(session.my_role(), x))
-            .collect_vec(),
+        let random_bits_z64 = Arc::new(
+            random_bits_z128
+                .iter()
+                .map(|b| Share::new(b.owner(), b.value().to_residuepoly64()))
+                .collect::<Vec<Share<ResiduePoly<Z64, EXTENSION_DEGREE>>>>(),
         );
 
-        let packed_random_bits_z64 = Arc::new(
-            pack_residue_poly(
-                &(random_bits_z64
-                    .into_iter()
-                    .map(|s| s.value())
-                    .collect::<Vec<_>>()),
-            )
-            .into_iter()
-            .map(|x| Share::new(session.my_role(), x))
-            .collect_vec(),
-        );
+        let secret_bit_vector = Arc::new(secret_bit_vector);
 
-        // NOTE: This won't work because we need to pack the Z::TWO over all the extension coeffs
-        // due to packing above
-        // i.e. have to reimplement XOR here for the packed case
-        let masked_packed_secret_bits = Bits::xor_list_secret_secret(
-            Arc::clone(&packed_secret_bits),
-            Arc::clone(&packed_random_bits_z64),
+        let masked_secret_bits = Bits::xor_list_secret_secret(
+            Arc::clone(&secret_bit_vector),
+            Arc::clone(&random_bits_z64),
             preproc,
             session,
         )
         .await?;
 
         // Open the masked bits
-        let opened_masked_bits_z64 = open_list(&masked_packed_secret_bits, session).await?;
+        let opened_masked_bits_z64 = open_list(&masked_secret_bits, session).await?;
 
         // Lift all the results to Z128 and transform to shares
-        let opened_masked_packed_bits_z128 = opened_masked_bits_z64
+        let opened_masked_bits_z128 = opened_masked_bits_z64
             .into_iter()
             .map(
                 |s| ResiduePoly::<Z128, EXTENSION_DEGREE> {
                     coefs: s.coefs.map(|c| Z128::from_u128(c.0 as u128)),
                 }, // Lift to Z128
             )
-            .collect::<Vec<ResiduePoly<Z128, EXTENSION_DEGREE>>>();
-
-        // Unpack
-        let opened_masked_bits_z128 = opened_masked_packed_bits_z128
-            .into_iter()
-            .flat_map(|packed_poly| packed_poly.coefs.map(ResiduePoly::from_scalar))
             .collect::<Vec<ResiduePoly<Z128, EXTENSION_DEGREE>>>();
 
         // Remove the mask by xoring, xor secret clear is only available for vec of vec
@@ -145,4 +123,153 @@ impl BitLift for SecureBitLift {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use itertools::Itertools;
+
+    use crate::{
+        algebra::{
+            base_ring::{Z128, Z64},
+            galois_rings::common::{Monomials, ResiduePoly},
+            structure_traits::ErrorCorrect,
+        },
+        execution::{
+            online::{
+                bit_lift::{BitLift, SecureBitLift},
+                preprocessing::{dummy::DummyPreprocessing, BitPreprocessing},
+                triple::open_list,
+            },
+            runtime::sessions::large_session::LargeSession,
+            sharing::share::Share,
+        },
+        malicious_execution::online::malicious_bit_lift::{
+            BitLiftAddError, BitLiftDrop, BitLiftWrongAmountTooFew, BitLiftWrongAmountTooMany,
+        },
+        networking::NetworkMode,
+        tests::helper::tests::{
+            execute_protocol_large_w_disputes_and_malicious, TestingParameters,
+        },
+    };
+
+    async fn test_bit_lift<const EXTENSION_DEGREE: usize, MaliciousBitLift: BitLift>(
+        params: TestingParameters,
+        num_bits: usize,
+        malicious_bit_lift: MaliciousBitLift,
+    ) where
+        ResiduePoly<Z64, EXTENSION_DEGREE>: ErrorCorrect + Monomials,
+        ResiduePoly<Z128, EXTENSION_DEGREE>: ErrorCorrect,
+    {
+        let mut task_honest = |mut session: LargeSession| async move {
+            let mut prep = DummyPreprocessing::new(42, &session);
+
+            // Pull out some random bits mod Z64 that we will lift
+            let bits_to_lift: Vec<Share<ResiduePoly<Z64, EXTENSION_DEGREE>>> =
+                prep.next_bit_vec(num_bits).unwrap();
+
+            assert_eq!(bits_to_lift.len(), num_bits);
+
+            let opened_bits_z64 = open_list(&bits_to_lift, &session).await.unwrap();
+
+            let lifted_bits = SecureBitLift::execute(bits_to_lift, &mut prep, &mut session)
+                .await
+                .unwrap();
+
+            let opened_bits_z128 = open_list(&lifted_bits, &session).await.unwrap();
+
+            assert_eq!(
+                opened_bits_z64.len(),
+                opened_bits_z128.len(),
+                "Number of opened bits should be the same before and after lifting"
+            );
+
+            for (b64, (idx, b128)) in opened_bits_z64
+                .iter()
+                .zip_eq(opened_bits_z128.iter().enumerate())
+            {
+                let b64_scalar = b64.to_scalar().unwrap().0;
+                // Sanity check we indeed started with bits
+                assert!(
+                    b64_scalar == 0 || b64_scalar == 1,
+                    "Input {idx} was not actually a bit, got {}",
+                    b64_scalar
+                );
+
+                assert_eq!(
+                    b64_scalar,
+                    b128.to_scalar().unwrap().0 as u64,
+                    "Lifted bit {idx} is not the same as original bit, expected {}, got {}",
+                    b64_scalar,
+                    b128.to_scalar().unwrap().0
+                );
+            }
+            Ok::<(), ()>(())
+        };
+
+        let mut task_malicious =
+            |mut session: LargeSession, _malicious_bitlift: MaliciousBitLift| async move {
+                let mut prep = DummyPreprocessing::new(42, &session);
+
+                // Pull out some random bits mod Z64 that we will lift
+                let bits_to_lift: Vec<Share<ResiduePoly<Z64, EXTENSION_DEGREE>>> =
+                    prep.next_bit_vec(num_bits).unwrap();
+
+                assert_eq!(bits_to_lift.len(), num_bits);
+
+                let _opened_bits_z64 = open_list(&bits_to_lift, &session).await;
+
+                let lifted_bits = MaliciousBitLift::execute(bits_to_lift, &mut prep, &mut session)
+                    .await
+                    .unwrap();
+
+                let _opened_bits_z128 = open_list(&lifted_bits, &session).await;
+
+                Ok::<(), ()>(())
+            };
+
+        let (results_honest, _results_malicious) =
+            execute_protocol_large_w_disputes_and_malicious::<
+                _,
+                _,
+                _,
+                _,
+                _,
+                ResiduePoly<Z64, EXTENSION_DEGREE>,
+                EXTENSION_DEGREE,
+            >(
+                &params,
+                &params.dispute_pairs,
+                &params.malicious_roles,
+                malicious_bit_lift,
+                NetworkMode::Async,
+                None,
+                &mut task_honest,
+                &mut task_malicious,
+            )
+            .await;
+        assert_eq!(
+            results_honest.len(),
+            params.num_parties - params.malicious_roles.len()
+        );
+        assert!(results_honest.iter().all(|res| res.1.is_ok()));
+    }
+
+    #[tokio::test]
+    async fn sunshine() {
+        let params = TestingParameters::init(4, 1, &[], &[], &[], false, None);
+        test_bit_lift::<4, _>(params, 10, SecureBitLift).await;
+    }
+
+    #[rstest::rstest]
+    async fn malicious_lift<B: BitLift + 'static>(
+        #[values(
+            BitLiftDrop,
+            BitLiftWrongAmountTooMany,
+            BitLiftWrongAmountTooFew,
+            BitLiftAddError
+        )]
+        malicious_bit_lift: B,
+    ) {
+        // Note: Malicious strategies above to do not depend on a "role to lie to"
+        let params = TestingParameters::init(4, 1, &[2], &[], &[], false, None);
+        test_bit_lift::<4, _>(params, 10, malicious_bit_lift).await;
+    }
+}

core/threshold/src/execution/online/triple.rs
@@ -131,6 +131,10 @@ pub async fn mult_list<Z: Ring + ErrorCorrect, Ses: BaseSessionHandles>(
     .await
 }
 
+// TODO: Calls to this should be replaced by calls to RobustOpen protocol.
+// This would be more consistent with the rest of the codebase and
+// allow for easier testing of malicious strategies.
+
 /// Opens a single secret
 pub async fn open<Z: Ring + ErrorCorrect, Ses: BaseSessionHandles>(
     to_open: Share<Z>,

core/threshold/src/execution/sharing/open.rs
@@ -500,7 +500,7 @@ async fn try_reconstruct_from_shares<Z: ErrorCorrect>(
                         values,
                         num_secrets,
                         party_id,
-                    )?;
+                    );
                     collected_shares += 1;
                 } else if let Err(e) = data {
                     tracing::warn!(

core/threshold/src/execution/sharing/shamir.rs
@@ -1,7 +1,6 @@
 use super::share::Share;
 use crate::algebra::poly::Poly;
 use crate::algebra::structure_traits::{ErrorCorrect, Ring, RingWithExceptionalSequence};
-use crate::error::error_handler::anyhow_error_and_log;
 use crate::error::error_handler::anyhow_error_and_warn_log;
 use crate::execution::runtime::party::Role;
 use itertools::EitherOrBoth::{Both, Left, Right};
@@ -206,58 +205,38 @@ where
 }
 
 /// Maps `values` into [ShamirSharings]s by pairwise adding each of these to each of the `sharings`.
-/// Furthermore, adds a zero-share for each share in `sharings` if there is not supplied enough `values`.
+/// Furthermore, adds a zero-share for each share in `sharings` if there is not supplied enough `values`,
+/// and discard extra shares if too many are given.
+/// __This supposes that sharings is always a source of truth !__
 /// This in turn means that there will be `num_values` shares in `sharings`.
 /// The function is useful to ensure that an indexable vector of Shamir shares exist.
 pub fn fill_indexed_shares<Z: Ring>(
     sharings: &mut [ShamirSharings<Z>],
-    values: Vec<Z>,
+    mut values: Vec<Z>,
     num_values: usize,
     party_id: Role,
-) -> anyhow::Result<()> {
-    if sharings.len() != num_values {
-        return Err(anyhow_error_and_log(format!(
-            "Number of sharings {} is not the expected amount {} from party {}",
-            sharings.len(),
-            num_values,
-            party_id
-        )));
-    }
-    if sharings.len() < values.len() {
-        return Err(anyhow_error_and_log(format!(
-            "Number of sharings {} is not {} as expected from party {}.",
-            sharings.len(),
+) {
+    if values.len() < num_values {
+        tracing::warn!(
+            "Received {} values from party {} but expected {}. Filling with 0s",
             values.len(),
+            party_id,
+            num_values
+        );
+    } else if values.len() > num_values {
+        tracing::warn!(
+            "Received more values than expected from party {}. Ignoring extra values.",
             party_id
-        )));
+        );
     }
-    let sharing_len = sharings.len();
-    let values_len = values.len();
-    let pair = values.into_iter().zip_longest(sharings.iter_mut());
-    for cur in pair {
-        match cur {
-            Both(v, sharing) => {
-                sharing.add_share(Share::new(party_id, v));
-            }
-            Left(_v) => {
-                // If a share is missing, we panic since we already checked the lengths in the start of the method
-                panic!(
-                    "There are {sharing_len} shares, but {values_len} values. There should not be more values than shares from party {party_id}."
-                );
-            }
-            Right(sharing) => {
-                // If a value is missing, then add a zero-share in liu of the value
-                tracing::warn!(
-                    "Received {} shares from {} but expected {}. Filling with 0s",
-                    values_len,
-                    party_id,
-                    num_values
-                );
-                sharing.add_share(Share::new(party_id, Z::ZERO));
-            }
-        }
+
+    values.resize(num_values, Z::ZERO);
+
+    // Use zip_eq as we just resized the values received to the expected length
+    let pair = values.into_iter().zip_eq(sharings.iter_mut());
+    for (v, sharing) in pair {
+        sharing.add_share(Share::new(party_id, v));
     }
-    Ok(())
 }
 
 /// Core algorithm for robust reconstructions which tries to reconstruct from a collection of shares

core/threshold/src/malicious_execution/mod.rs
@@ -15,6 +15,7 @@ pub mod large_execution {
     pub mod malicious_vss;
 }
 pub mod online {
+    pub mod malicious_bit_lift;
     pub mod malicious_gen_bits;
     pub mod malicious_reshare;
     pub mod preprocessing {