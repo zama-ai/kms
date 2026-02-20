670f9f41ecadae98ac3044ce94300d1cac177982
David Palm <dvdplm@gmail.com>
2026-02-20 10:12:29 +0100

    Add skeleton threshold-algebra crate

---
+++ core/threshold-algebra/src/lib.rs
@@ -0,0 +1,14 @@
+// pub mod base_ring;
+// pub mod bivariate;
+// pub mod error_correction;
+// pub mod galois_fields;
+// pub mod galois_rings;
+// pub mod poly;
+// pub mod structure_traits;
+// pub mod syndrome;
+
+// #[track_caller]
+// pub(crate) fn anyhow_error_and_log<S: AsRef<str> + fmt::Display>(msg: S) -> anyhow::Error {
+//     tracing::error!("Error in {}: {}", Location::caller(), msg);
+//     anyhow!("Error in {}: {}", Location::caller(), msg)
+// }
