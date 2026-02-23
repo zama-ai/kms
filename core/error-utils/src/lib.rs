8a4231d5c2db782c36de3f93f43bd930d63ac978
David Palm <dvdplm@gmail.com>
2026-02-23 16:25:27 +0100

    Clean up error-utils

--- core/error-utils/src/lib.rs
+++ core/error-utils/src/lib.rs
@@ -21,30 +21,24 @@ pub fn log_error_wrapper<S: AsRef<str> + fmt::Display>(msg: S) -> S {
     msg
 }
 
+#[cfg(test)]
 mod tests {
-    #[cfg(test)]
-    #[tracing_test::traced_test]
-    #[test]
-    #[allow(clippy::unnecessary_literal_unwrap, clippy::useless_format)]
-    fn test_log() {
-        use super::anyhow_error_and_log;
+    use super::{anyhow_error_and_log, anyhow_error_and_warn_log};
 
-        let _err = Err::<(), anyhow::Error>(anyhow_error_and_log(format!("(test_log), msg",)));
-        assert!(logs_contain("src/error/error_handler.rs"));
+    #[test]
+    #[tracing_test::traced_test]
+    fn test_log() {
+        let _ = anyhow_error_and_log("(test_log), msg");
+        assert!(logs_contain("src/lib.rs"));
         assert!(logs_contain("(test_log), msg"));
         assert!(logs_contain("Error in"));
     }
 
-    #[cfg(test)]
     #[tracing_test::traced_test]
     #[test]
-    #[allow(clippy::unnecessary_literal_unwrap, clippy::useless_format)]
     fn test_warn_log() {
-        use super::anyhow_error_and_warn_log;
-
-        let _err =
-            Err::<(), anyhow::Error>(anyhow_error_and_warn_log(format!("(test_warn_log), msg",)));
-        assert!(logs_contain("src/error/error_handler.rs"));
+        let _ = anyhow_error_and_warn_log("(test_warn_log), msg");
+        assert!(logs_contain("src/lib.rs"));
         assert!(logs_contain("(test_warn_log), msg"));
         assert!(logs_contain("Warning in"));
     }
