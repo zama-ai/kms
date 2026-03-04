0ddf67abfc161be0a030daae368e170a4a653337
David Palm <dvdplm@gmail.com>
2026-03-04 13:53:26 +0100

    Docs for ProtocolDescription

--- core/threshold-types/src/protocol.rs
+++ core/threshold-types/src/protocol.rs
@@ -1,5 +1,4 @@
-// TODO(dp): document/describe this
-
+/// Provide a short description of a protocol, indented with `depth` number of spaces
 pub trait ProtocolDescription {
     const INDENT_STRING: &str = "   ";
     fn protocol_desc(depth: usize) -> String;
