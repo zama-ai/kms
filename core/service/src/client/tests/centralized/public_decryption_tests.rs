740977339cdc825ea21aa25478c2eaeb98e600fd
Kelong Cong <kc1212@users.noreply.github.com>
2026-03-27 13:40:14 +0100

    chore: add eip-712 validation in public decryption tests (#478) (#485)
    Closes zama-ai/kms-internal#2949

--- core/service/src/client/tests/centralized/public_decryption_tests.rs
+++ core/service/src/client/tests/centralized/public_decryption_tests.rs
@@ -266,6 +266,10 @@ pub(crate) async fn run_decryption_centralized(
     // go through all requests and check the corresponding responses
     for req in &reqs {
         let req_id = req.request_id.as_ref().unwrap();
+
+        // make sure domain exists since it needs to be used for external signature verification
+        assert!(req.domain.is_some());
+
         let responses: Vec<_> = resp_response_vec
             .iter()
             .filter_map(|resp| {
