e2e672657eeb50a85266a3e3a0b0cf878d9de4af
Simon Eudeline <simon.eudeline@zama.ai>
2026-04-10 14:21:10 +0200

    fix: wasm compilation

--- core/service/src/consts.rs
+++ core/service/src/consts.rs
@@ -2,6 +2,7 @@
 use crate::engine::base::derive_request_id;
 #[cfg(feature = "non-wasm")]
 use kms_grpc::{EpochId, RequestId, identifiers::ContextId};
+#[cfg(feature = "non-wasm")]
 use std::sync::LazyLock;
 use threshold_execution::tfhe_internals::parameters::{
     BC_PARAMS_SNS, DKGParams, PARAMS_TEST_BK_SNS,
