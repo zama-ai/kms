1917f8dab7446cc3d52213ef960e2aeddf4c5f0f
Kelong Cong <kc1212@users.noreply.github.com>
2026-04-23 10:42:31 +0200

    chore: upgrade tfhe-rs to 1.6

--- core/service/src/vault/storage/crypto_material/tests.rs
+++ core/service/src/vault/storage/crypto_material/tests.rs
@@ -818,7 +818,7 @@ fn setup_threshold_store(
 
     let fhe_key_set = keyset.public_keys.clone();
 
-    let (integer_server_key, _, _, _, sns_key, _, _, _) =
+    let (integer_server_key, _, _, _, sns_key, _, _, _, _) =
         keyset.public_keys.server_key.clone().into_raw_parts();
 
     let threshold_fhe_keys = ThresholdFheKeys {
