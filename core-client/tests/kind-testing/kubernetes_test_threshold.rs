bbad047686fef0944e85197aeab7c6d90bd9ed0f
David Palm <dvdplm@gmail.com>
2026-04-27 10:45:49 +0200

    chore: k8 tests renamed and updated

--- core-client/tests/kind-testing/kubernetes_test_threshold.rs
+++ core-client/tests/kind-testing/kubernetes_test_threshold.rs
@@ -190,7 +190,7 @@ impl K8sTestContext {
             parallel_requests: 1,
             ciphertext_output_path: Some(cipher_path.clone()),
             inter_request_delay_ms: 0,
-            compressed_keys: false,
+            uncompressed_keys: false,
             extra_data: None,
         }))
         .await;
