0005ca804bd720eb744bb21b3801323765f0c485
Titouan Tanguy <titouan.tanguy@zama.ai>
2026-05-29 17:55:37 +0200

    chore: mistake in hash

--- core/experiments/test_scripts/tfhe_reproducible_small_session_malicious.sh
+++ core/experiments/test_scripts/tfhe_reproducible_small_session_malicious.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SESSION_TYPE="small"
-EXPECTED_KEY_HASH="82fe65a87954f9fbccb936c74d29d47ffb45b2ba2cd66c92efc92c18cd3a667e"
+EXPECTED_KEY_HASH="82fe65a87954f9fbccb936c74d29d47ffb45b2ba2cd66c92efc92c18cd3a6679"
 EXPECTED_RESHARED_KEY_HASH="d7d223bb1868b3fa221d4cfa08e1a7ab2c2a9e2c618da67bb811ec82ed8589ed"
 DDEC_MODES="noise-flood-small bit-dec-small"
 MAIN_PATH="./temp/tfhe_small_reproducible_malicious"
