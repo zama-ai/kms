64063f5924981cd2e1979f1b336f9826f7f0730f
Tore Frederiksen <tore.frederiksen@zama.ai>
2026-09-22 14:36:01 +0200

    fix: bad merge

--- core/service/src/backup/custodian.rs
+++ core/service/src/backup/custodian.rs
@@ -1,3 +1,4 @@
+use crate::backup::BACKUP_PKE_SCHEME;
 use crate::backup::operator::DSEP_BACKUP_MATERIAL;
 use crate::cryptography::{
     encryption::{HasPkeScheme, UnifiedPrivateEncKey, UnifiedPublicEncKey},
